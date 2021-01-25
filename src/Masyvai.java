7fd614b4c7b7c7208f2336d693f402cf8f54fe20
InetaVei <74532995+InetaVei@users.noreply.github.com>
2021-01-25 21:57:48 +0200

    Masyvai

---
+++ src/Masyvai.java
@@ -0,0 +1,17 @@
+public class Masyvai {
+
+    public static void main(String[] args) {
+
+        int x = 100;   // tiek x tiek y rodo i skirtingas atminties lasteles. Todel nematome pakitimu.
+        int y = x;
+
+        x = 200;
+        System.out.println(y);
+
+        int[] masyvas = { 1, 2, 3 };   // masyve rodoma i ta pacia viena lastele, todel matome pakitimus.
+        int[] masyvas2 = masyvas;
+
+        masyvas[0] = 5;
+        System.out.println(masyvas2[0]);
+    }
+}
