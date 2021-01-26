6a2b6e44ac809386f9059df917c4f8469ef2c47f
InetaVei <74532995+InetaVei@users.noreply.github.com>
2021-01-26 15:56:54 +0200

    masyvai2

---
+++ src/Masyvai2.java
@@ -0,0 +1,14 @@
+public class Masyvai2 {
+    public static void main(String[] args) {
+
+        String[] s = { "A", "A1", "B", "C", "A3"};
+        int skaicius = 0;
+//        for (int i = 0; i < s.length; i++) {
+//            String elementas = s[i];
+
+        for (String elementas : s) {  // tas dvitaštis atspindi zodeli 'IS', elementas is skaiciu masyvo
+            if (elementas.charAt(0) == 'A') skaicius++;
+        }
+        System.out.println("Viso prasideda A raide yra " + skaicius);
+    }
+}
