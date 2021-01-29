ae6c885a41c79e007d76d3195f009c5ab60c1c2f
InetaVei <74532995+InetaVei@users.noreply.github.com>
2021-01-29 11:19:30 +0200

    mokiniu uzdavinys

--- src/paskaita05/U1.java
+++ src/paskaita05/U1.java
@@ -8,26 +8,16 @@ public class U1 {
 
         System.out.println(U1.class.getName());  // cia issikvieciamas pilnas klases pavadinimas
 
+        Mokinys[] mokiniai = readFromDB();  // mokiniu masyvas nuskaitomas is duomenu bazes
+
         System.out.println("Pradiniai duomenys");
         // paskaita05.intro.Mokinys[] mokiniai = new paskaita05.intro.Mokinys[5];  // taip nurodomas pilnas kelias su kokiom klasem mes norim dirbti. Toks budas ner patogus, bet galimas
-        Mokinys[] mokiniai = new Mokinys[]{
-                new Mokinys(),
-                new Mokinys("Paula", 1),
-                new Mokinys("Nora", 2),
-                new Mokinys("Mila", 1),
-                new Mokinys("Saule", 3),
-                new Mokinys("Nina")
-        };
-        mokiniai[0].name = "Nele";
-        mokiniai[0].level = 3;
 
         print(mokiniai);
 
         //sulygiuoti pagal klase ir varda
         for (int i = 0; i < mokiniai.length - 1; i++) {
-
             for (int j = i + 1; j < mokiniai.length; j++) {
-
                 if (compare(mokiniai[i], mokiniai[j]) > 0) {
                     Mokinys tmp = mokiniai[i];
                     mokiniai[i] = mokiniai[j];
@@ -39,7 +29,6 @@ public class U1 {
 //                    mokiniai[i] = mokiniai[j];
 //                    mokiniai[j] = tmp;
             }
-
         }
 
         System.out.println("Sulygiuota");
@@ -52,16 +41,29 @@ public class U1 {
 
     static void print(Mokinys[] mokiniai) {   // metodas print
         for (Mokinys m : mokiniai) {
-            System.out.println(m.name + " " + m.level);
+            System.out.println(m.getName() + " " + m.getLevel());
         }
     }
 
-
-
     static int compare(Mokinys m1, Mokinys m2) {
-        if (m1.level < m2.level) return -1;
-        if (m1.level > m2.level) return 1;
-        return m1.name.compareTo(m2.name);
+        if (m1.getLevel() < m2.getLevel()) return -1;
+        if (m1.getLevel() > m2.getLevel()) return 1;
+        return m1.getName().compareTo(m2.getName());
+    }
+
+    static Mokinys[] readFromDB() {    // masyvas nuskaitomas is duomenu bazes
+        Mokinys[] mokiniai = new Mokinys[]{
+                new Mokinys(),
+                new Mokinys("Paula", 1),
+                new Mokinys("Nora", 2),
+                new Mokinys("Mila", 1),
+                new Mokinys("S", 3),
+                new Mokinys("Nina")
+        };
+        mokiniai[0].setName(null);
+        mokiniai[0].setLevel(0);
+
+        return mokiniai;
     }
 
 }
--- src/paskaita05/intro/Mokinys.java
+++ src/paskaita05/intro/Mokinys.java
@@ -2,19 +2,42 @@ package paskaita05.intro;
 
 public class Mokinys {   // ale projekto aprasas apacioj, ka tas mokinys tures
 
-    public String name;   // pagal nutylejima reiksme - null.
-    public int level;     // pagal nutylejima reiksme - 0
+    private String name;   // pagal nutylejima reiksme - null.
+    private int level;     // pagal nutylejima reiksme - 0
 
     public Mokinys(String name, int level) {   // mano sukurtas konstruktorius. Metodas, kurio pavadinimas atitinka klases pavadinima.
+        this(name);  // taip kvieciamas antrasis konstruktorius
+//        if (name == null || name.length() <= 1) {
+//            throw new IllegalArgumentException("Neteisingas vardas");
+//        }
         this.name = name;        // konstruktoriaus parametras
         this.level = level;       // konstruktoriaus parametras
     }
 
     public Mokinys(String name) {
-        this.name = name;
+        setName(name);
         level = 1;
     }
     public Mokinys(){
         level = 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        if (name == null || name.length() <= 1) {
+            throw new IllegalArgumentException("Neteisingas vardas");
+        }
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
     }
 }
